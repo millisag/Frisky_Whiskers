e7a50b375ae4095ef247a212fe00d4f8773ad1e1
Millisa G <135849424+millisag@users.noreply.github.com>
2025-04-20 23:11:16 -0400

    Added header image, styling

--- src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivityListComponent } from "../../../features/activity-list/activity-list.component";
 import { TreatListComponent } from "../../../features/treat-list/treat-list.component";
+import { HeaderComponent } from "../../../shared/header/header.component";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [ActivityListComponent, TreatListComponent],
+  imports: [ActivityListComponent, TreatListComponent, HeaderComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
